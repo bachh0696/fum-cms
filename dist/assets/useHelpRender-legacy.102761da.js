9ceb8ce0c12f73899dbbac0f1cb45cca7df9a3c0
hoangbach0696 <hoangbach0696@gmail.com>
2023-02-24 00:01:30 +0700

    update

---
+++ dist/assets/useHelpRender-legacy.102761da.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy.7a415fa2.js"],(function(e,t){"use strict";var r,n,c;return{setters:[e=>{r=e.c,n=e.K,c=e.M}],execute:function(){e("r",(e=>e?.length>30?r(n,{title:e,children:r("div",{children:c.exports.truncate(e)})}):e))}}}));
---
+++ dist/assets/useHelpRender.5a7b6bf1.js
@@ -0,0 +1,1 @@
+import{c as n,K as o,M as s}from"./index.8e71033d.js";const c=r=>(r==null?void 0:r.length)>30?n(o,{title:r,children:n("div",{children:s.exports.truncate(r)})}):r;export{c as r};
