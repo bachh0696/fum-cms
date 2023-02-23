b2798e9944dfd0a0a0d4140786d14ee9c5b87174
hoangbach0696 <hoangbach0696@gmail.com>
2023-02-23 23:44:21 +0700

    update

--- dist/assets/FormInputSearch.45cb8127.js
+++
@@ -1,1 +0,0 @@
-import{R as c,c as n,O as d,W as i,X as m}from"./index.0dd042c3.js";const l=({id:e,label:r="Search",placeholder:t="",size:a="small",className:o,...s})=>n(d,{id:e,label:r,placeholder:t,variant:"outlined",className:o,size:a,InputProps:{endAdornment:n(i,{position:"end",children:n(m,{children:"search"})})},...s});c.memo(l);
